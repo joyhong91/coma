34b0b4cbf00722c078ea25b4bc662d1cbb1c6e32
joyhong91 <remonkp@gmail.com>
2020-04-02 18:26:12 +0900

    add intro

---
+++ components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <img src="~/assets/imgs/main_logo.jpg" class="main_logo"/>
+</template>
+<style>
+.main_logo {
+  width: 100%;
+  height: 100%;
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
---
+++ pages/intro.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="intro_section">
+        <div class="img_wrapper">
+            <img src="~/assets/imgs/intro/slide_1.jpg" class="intro_img img1"/>
+        </div>    
+    </div>
+</template>
+
+<style scoped>
+.intro_section {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #000;
+    height: 100vh;
+}
+
+.intro_img{
+    width: 100%;
+}
+</style>
